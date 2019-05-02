c0264848be6f5f942e11ca59e48a5251108b0b99
ccecoursedev <ccecrsdv@mcmaster.ca>
2019-05-02 13:31:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ document.getElementById('content-library').innerHTML = '<app-root></app-root>';
 
 var script1 = document.createElement("script");
 script1.type = "text/javascript";
-script1.src = 'https://rharor.github.io/content-library-app/runtime.js';
+script1.src = 'https://RickyHaror.github.io/content-library-app/runtime.js';
 document.getElementsByTagName("head")[0].appendChild(script1);
 
 var script2 = document.createElement("script");
 script2.type = "text/javascript";
-script2.src = 'https://rharor.github.io/content-library-app/polyfills.js';
+script2.src = 'https://RickyHaror.github.io/content-library-app/polyfills.js';
 document.getElementsByTagName("head")[0].appendChild(script2);
 
 var script3 = document.createElement("script");
 script3.type = "text/javascript";
-script3.src = 'https://rharor.github.io/content-library-app/main.js';
+script3.src = 'https://RickyHaror.github.io/content-library-app/main.js';
 document.getElementsByTagName("head")[0].appendChild(script3);
