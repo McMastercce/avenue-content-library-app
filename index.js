c88800107cde22a7ea68964a2fa1c312f1ec3e46
ccecoursedev <ccecrsdv@mcmaster.ca>
2019-05-03 07:45:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ window.databaseRepoPath = 'avenue-content-library-database';
 document.getElementById('content-library').innerHTML = '<app-root></app-root>';
 
 var script1 = document.createElement("script");
-script1.type = "text/javascript";
 script1.src = 'https://RickyHaror.github.io/content-library-app/runtime.js';
-document.getElementsByTagName("head")[0].appendChild(script1);
+document.getElementsByTagName("body")[0].appendChild(script1);
 
 var script2 = document.createElement("script");
-script2.type = "text/javascript";
 script2.src = 'https://RickyHaror.github.io/content-library-app/polyfills.js';
-document.getElementsByTagName("head")[0].appendChild(script2);
+document.getElementsByTagName("body")[0].appendChild(script2);
 
 var script3 = document.createElement("script");
-script3.type = "text/javascript";
 script3.src = 'https://RickyHaror.github.io/content-library-app/main.js';
-document.getElementsByTagName("head")[0].appendChild(script3);
+document.getElementsByTagName("body")[0].appendChild(script3);
